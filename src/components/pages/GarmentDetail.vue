7acdf50538df046827367827de1e71071fa9c6fe
Bram van Osta <bramvanosta@gmail.com>
2017-02-23 15:58:13 +0100

    Some CSS on GarmentDetail page

--- src/components/pages/GarmentDetail.vue
+++ src/components/pages/GarmentDetail.vue
@@ -2,10 +2,10 @@
     <main class="garment-detail__container">
         <h2 class="garment-detail__title">{{name}}</h2>
         <div class="mdc-layout-grid">
-            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
                 <div style="background: red; height: 150px"></div>
             </div>
-            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-8">
                 <div class="garment-detail__info-box">
                     <span class="garment-detail__info-value">Kids</span>
                     <span class="garment-detail__info-label">Category</span>
@@ -35,4 +35,17 @@
 </script>
 
 <style>
+    .garment-detail__info-box {
+        padding: 15px 0;
+    }
+
+    .garment-detail__info-box:not(:last-of-type) {
+        border-bottom: 1px solid grey;
+    }
+
+    .garment-detail__info-value {
+        display: block;
+        font-size: 2.4rem;
+        font-weight: bold;
+    }
 </style>
