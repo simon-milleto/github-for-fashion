3abcc945e2ce21da7eb4b88120d784d0e728f560
Bram van Osta <bramvanosta@gmail.com>
2017-02-23 15:52:35 +0100

    Started working on GarmentDetail page

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <div id="app">
-    <g-header></g-header>
-    <router-view></router-view>
-    <g-footer></g-footer>
-  </div>
+    <div id="app">
+        <g-header></g-header>
+        <router-view></router-view>
+        <g-footer></g-footer>
+    </div>
 </template>
 
 <script>
-import GHeader from './components/GHeader.vue'
-import GFooter from './components/GFooter.vue'
+  import GHeader from './components/GHeader.vue'
+  import GFooter from './components/GFooter.vue'
 
-export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
-  },
-  components: {
-    GHeader,
-    GFooter
+  export default {
+      name: 'app',
+      data () {
+          return {
+              msg: 'Welcome to Your Vue.js App'
+          }
+      },
+      components: {
+          GHeader,
+          GFooter
+      }
   }
-}
 </script>
 
 <style>
---
+++ src/components/pages/GarmentDetail.vue
@@ -0,0 +1,38 @@
+<template>
+    <main class="garment-detail__container">
+        <h2 class="garment-detail__title">{{name}}</h2>
+        <div class="mdc-layout-grid">
+            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+                <div style="background: red; height: 150px"></div>
+            </div>
+            <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+                <div class="garment-detail__info-box">
+                    <span class="garment-detail__info-value">Kids</span>
+                    <span class="garment-detail__info-label">Category</span>
+                </div>
+                <div class="garment-detail__info-box">
+                    <span class="garment-detail__info-value">S M L XL</span>
+                    <span class="garment-detail__info-label">Sizes</span>
+                </div>
+                <div class="garment-detail__info-box">
+                    <span class="garment-detail__info-value">2400</span>
+                    <span class="garment-detail__info-label">Favourites</span>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+    export default {
+        name: 'garment-detail',
+        data() {
+            return {
+                name: 'Garment project title'
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
