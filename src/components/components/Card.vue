f1c3892548e552b493e3639df5fa2821e13f4977
Guillaume Darricau <g.darricau@hotmail.com>
2017-02-23 15:59:18 +0100

    Add card component

---
+++ src/components/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
+        <div class="mdc-card">
+            <div class="mdc-card__media">
+                <img src="http://pukkaind.biz/images/categories/16Wool%20varsity%20Letterman%20jacket%20Royal-white.jpg" width='100%' height='240'>
+            </div>
+            <div class="mdc-card__primary">
+                <div class="mdc-card__title">{{data.title}}</div>
+                <div class="mdc-card__subtitle">{{data.username}} on {{data.date}}</div>
+            </div>
+            <div class="mdc-card__supporting-text">
+                <div>{{data.city}} - {{data.country}}</div>
+                <div>{{data.contributers}} Contributers</div>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+</style>
+<script>
+    export default{
+        name: 'card',
+        props: ['data']
+    }
+</script>
--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,12 +1,40 @@
 <template>
     <div id="home">
-        CONTENU
+        <div class="mdc-layout-grid">
+            <card v-for="card in cards" :data="card"></card>
+        </div>
     </div>
 </template>
 
 <script>
+    import Card from '../components/Card.vue'
     export default {
-        name: 'home'
+      name: 'home',
+      data () {
+        return {
+          cards: [
+            {
+              title: 'Slip léopard zero waste',
+              date: '12/07/16',
+              username: 'Simon Milleto',
+              city: 'Bordeaux',
+              country: 'France',
+              contributers: '12'
+            },
+            {
+              title: 'Blouson Firefox',
+              date: '12/07/18',
+              username: 'DavidB',
+              city: 'Bordeaux',
+              country: 'France',
+              contributers: '2'
+            }
+          ]
+        }
+      },
+      components: {
+        Card
+      }
     }
 </script>
 
