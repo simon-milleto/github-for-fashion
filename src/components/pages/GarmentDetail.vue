52650498687ba8a1b38e11f77d7bd7840fea3d5a
Bram van Osta <bramvanosta@gmail.com>
2017-04-06 16:59:06 +0200

    Adds CommitInfo and Loader components

--- src/components/pages/GarmentDetail.vue
+++ src/components/pages/GarmentDetail.vue
@@ -1,69 +1,66 @@
 <template>
   <div class="garment-detail">
-    <h2 class="garment-detail__title">{{garment.title}}</h2>
-    <div class="mdc-layout-grid">
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-        <slideshow></slideshow>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
-        <info-box v-for="info in garment.infos" :value="info.value" :label="info.label"></info-box>
-      </div>
-    </div>
-    <div class="separator"></div>
-    <div class="mdc-layout-grid">
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-        <span class="garment-detail__label">Creator</span>
-        <span>{{garment.creator}}</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-        <span class="garment-detail__label">Iteration n°</span>
-        <span v-if="garment.commit">{{garment.commit.changes}}</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-        <span class="garment-detail__label">Type</span>
-        <span>{{garment.type}}</span>
-      </div>
-    </div>
-    <div class="mdc-layout-grid">
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-        <span class="garment-detail__label">Creation date</span>
-        <span>{{garment.creation_date | moment}}</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-        <span class="garment-detail__label">Platform ref.</span>
-        <span>{{garment.reference}}</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-        <span class="garment-detail__label">Pattern status</span>
-        <span>{{garment.status}}</span>
-      </div>
-    </div>
-    <div class="garment-detail__project-info mdc-layout-grid">
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-          <span class="garment-detail__commit-value" v-if="garment.commit">{{garment.commit.proposals.length}}</span>
-          <span class="garment-detail__commit-label">Change Proposals</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-          <span class="garment-detail__commit-value" v-if="garment.commit">{{garment.commit.changes}}</span>
-          <span class="garment-detail__commit-label">Changes</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-          <span class="garment-detail__commit-value">{{garment.licence}}</span>
-          <span class="garment-detail__commit-label">Licence</span>
-      </div>
-      <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
-          <span class="garment-detail__commit-value" v-if="garment.commit">{{garment.commit.contributors}}</span>
-          <span class="garment-detail__commit-label">Contributors</span>
-      </div>
-    </div>
-    <div class="garment-detail__description">
-      <p class="garment-detail__description-label">Project details</p>
-      <p>{{garment.description}}</p>
-    </div>
-    <div class="garment-detail__download">
-      <p class="garment-detail__download-label">Download</p>
+    <loader v-if="!dataIsLoaded"></loader>
+    <div class="garment-detail__content" v-if="dataIsLoaded">
+      <h2 class="garment-detail__title">{{garment.title}}</h2>
       <div class="mdc-layout-grid">
-        <download-box v-for="file in garment.files" :type="file.filetype" :available="file.available" :url="file.url"></download-box>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+          <slideshow></slideshow>
+        </div>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-6">
+          <info-box v-for="info in garment.infos"
+                    :value="info.value"
+                    :label="info.label">
+          </info-box>
+        </div>
+      </div>
+      <div class="separator"></div>
+      <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
+          <span class="garment-detail__label">Creator</span>
+          <span>{{garment.creator}}</span>
+        </div>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
+          <span class="garment-detail__label">Iteration n°</span>
+          <span v-if="garment.commit">{{garment.commit.changes}}</span>
+        </div>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
+          <span class="garment-detail__label">Type</span>
+          <span>{{garment.type}}</span>
+        </div>
+      </div>
+      <div class="mdc-layout-grid">
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
+          <span class="garment-detail__label">Creation date</span>
+          <span>{{garment.creation_date | moment}}</span>
+        </div>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
+          <span class="garment-detail__label">Platform ref.</span>
+          <span>{{garment.reference}}</span>
+        </div>
+        <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-3">
+          <span class="garment-detail__label">Pattern status</span>
+          <span>{{garment.status}}</span>
+        </div>
+      </div>
+      <commit-info :numberOfProposals="garment.commit.proposals.length"
+                  :changes="garment.commit.changes"
+                  :licence="garment.licence"
+                  :contributors="garment.commit.contributors">
+      </commit-info>
+      <div class="garment-detail__description">
+        <p class="garment-detail__description-label">Project details</p>
+        <p>{{garment.description}}</p>
+      </div>
+      <div class="garment-detail__download">
+        <p class="garment-detail__download-label">Download</p>
+        <div class="mdc-layout-grid">
+          <download-box v-for="file in garment.files"
+                        :type="file.filetype"
+                        :available="file.available"
+                        :url="file.url">
+          </download-box>
+        </div>
       </div>
     </div>
   </div>
@@ -75,14 +72,24 @@
 
   import InfoBox from '../components/InfoBox.vue';
   import DownloadBox from '../components/DownloadBox.vue';
+<<<<<<< HEAD
   import Slideshow from '../components/Slideshow.vue';
+=======
+  import CommitInfo from '../components/CommitInfo.vue';
+  import Loader from '../components/Loader.vue';
+>>>>>>> Adds CommitInfo and Loader components
 
   export default {
     name: 'garment-detail',
     components: {
       InfoBox,
       DownloadBox,
+<<<<<<< HEAD
       Slideshow,
+=======
+      CommitInfo,
+      Loader,
+>>>>>>> Adds CommitInfo and Loader components
     },
     beforeCreate() {
       const id = this.$route.params.id;
@@ -90,12 +97,14 @@
       axios.get(`http://localhost:3000/garments/${id}`)
         .then((response) => {
           this.garment = response.data;
+          this.dataIsLoaded = true;
         })
         .catch(error => console.error(error));
     },
     data() {
       return {
         garment: {},
+        dataIsLoaded: false,
       };
     },
     filters: {
@@ -112,6 +121,7 @@
     font-weight: 600;
   }
 
+<<<<<<< HEAD
   .garment-detail__project-info {
     padding: 20px;
     border: 1px solid grey;
@@ -126,6 +136,12 @@
   .garment-detail__download {
     margin: 25px 0;
   }
+=======
+    .garment-detail__description,
+    .garment-detail__download {
+        margin: 25px 0;
+    }
+>>>>>>> Adds CommitInfo and Loader components
 
   .garment-detail__description-label,
   .garment-detail__download-label {
