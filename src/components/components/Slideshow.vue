ac78f1670b6d4cb115f9d914be36adc840d98e5a
rthoreau <romain.thoreau@laposte.net>
2017-04-06 16:35:49 +0200

    modif dupplication contenu

--- src/components/components/Slideshow.vue
+++ src/components/components/Slideshow.vue
@@ -1,78 +1,71 @@
 <template>
-	<div id="slideshow">
-		<ul>
-			<li v-for="p in pictures">
-				<img v-bind:src="p.url" v-bind:alt="p.alt" @click="setCurrent(p.id)" v-bind:class="'picture ' + (p.active ? 'active' : '')">
-			</li>
-		</ul>
-		<img v-bind:src="currentPicture.url" v-bind:alt="currentPicture.alt" id="show">
-	</div>
+  <div id="slideshow">
+    <ul>
+      <li v-for="(p, i) in pictures">
+        <img v-bind:src="p.url" v-bind:alt="p.alt" @click="setCurrent(i)" v-bind:class="'picture ' + (p.active ? 'active' : '')">
+      </li>
+    </ul>
+    <img v-bind:src="pictures[currentPictureIndex].url" v-bind:alt="pictures[currentPictureIndex].alt" id="show">
+  </div>
 </template>
 
 <script>
-	export default {
-	  	name: 'Slideshow',
-	  	data() {
-			return {
-				pictures: [{
-					"id":1,
-					"url": "http://cdn.tutorialzine.com/wp-content/uploads/2016/03/css-variables.jpg",
-					"alt": "beautiful picture",
-					"active":true
-				},
-				{
-					"id":2,
-					"url": "http://cdn.tutorialzine.com/wp-content/uploads/2016/02/great-looking-pricing-tables-150x150.jpg",
-					"alt": "great picture",
-					"active":false
-				},
-				{
-					"id":3,
-					"url": "http://cdn.tutorialzine.com/wp-content/uploads/2015/12/creating-your-first-desktop-app-with-electron.png",
-					"alt": "bad picture",
-					"active":false
-				}],
-				currentPicture:{
-					"url": "http://cdn.tutorialzine.com/wp-content/uploads/2016/03/css-variables.jpg",
-					"alt": "beautiful picture",
-				}
-			}
-		},
-		methods: {
+  export default {
+    name: 'Slideshow',
+    data() {
+      return {
+        pictures: [{
+          "url": "http://cdn.tutorialzine.com/wp-content/uploads/2016/03/css-variables.jpg",
+          "alt": "beautiful picture",
+          "active":true
+        },
+        {
+          "url": "http://cdn.tutorialzine.com/wp-content/uploads/2016/02/great-looking-pricing-tables-150x150.jpg",
+          "alt": "great picture",
+          "active":false
+        },
+        {
+          "url": "http://cdn.tutorialzine.com/wp-content/uploads/2015/12/creating-your-first-desktop-app-with-electron.png",
+          "alt": "bad picture",
+          "active":false
+        }],
+        currentPictureIndex:0
+      }
+    },
+    methods: {
 
-			setCurrent(x){
-				this.currentPicture['url']=this.pictures[x - 1].url;
-				this.currentPicture['alt']=this.pictures[x - 1].alt;
-				this.pictures.forEach( function(pic){
-					pic['active'] = false;
-				});
-				this.pictures[x - 1].active = true;
-			}
-		}
-	}
+      setCurrent(x){
+        this.currentPictureIndex=x;
+        this.pictures.forEach( function(pic){
+          pic['active'] = false;
+        });
+        this.pictures[x].active = true;
+      }
+    }
+  }
 
 </script>
 <style lang="scss" scoped>
-	#slideshow{
-		#show{
-			width:100%;
-		}
-		ul{
-			margin:0;
-			padding:0;
-		}
-		li{
-			list-style-type:none;
-			display:inline-block;
-		}
-		.picture{
-			width:40px;
-			height:40px;
-			object-fit:cover;
-			box-sizing:border-box;
-			&.active{
-				border:4px solid #00aadd;
-			}
-		}
-	}
+  #slideshow{
+    #show{
+      width:100%;
+    }
+    ul{
+      margin:0;
+      padding:0;
+    }
+    li{
+      list-style-type:none;
+      display:inline-block;
+    }
+    .picture{
+      width:40px;
+      height:40px;
+      object-fit:cover;
+      box-sizing:border-box;
+      &.active{
+        border:4px solid #00aadd;
+      }
+    }
+  }
 </style>
