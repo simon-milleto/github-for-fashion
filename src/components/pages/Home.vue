04ae34d842afac35e1470ad06217e354a3e5e7fe
simon-milleto <contact@simonmilleto.com>
2017-04-06 10:51:42 +0200

    Main card

--- src/components/pages/Home.vue
+++ src/components/pages/Home.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="home">
+        <main-card :data="mainCard"></main-card>
         <div class="mdc-layout-grid">
             <card v-for="card in cards" :data="card"></card>
         </div>
@@ -8,6 +9,7 @@
 
 <script>
     import Card from '../components/Card.vue'
+    import MainCard from '../components/MainCard.vue'
     export default {
       name: 'home',
       data () {
@@ -29,11 +31,21 @@
               country: 'France',
               contributers: '2'
             }
-          ]
+          ],
+          mainCard: {
+                  title: 'Patron Jacket zero waste',
+                  artist: 'BramyVony',
+                  description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, '+
+                   'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam',
+                  img: 'https://www.outdoorresearch.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/244897_0936.jpg',
+                  location: 'Bordeaux - France',
+                  contributor: 2
+                }
         }
       },
       components: {
-        Card
+        Card,
+        MainCard
       }
     }
 </script>
