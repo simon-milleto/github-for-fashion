17edf6e0bb4d9a6954eb57f4e51ae5c07dc85ab0
rthoreau <romain.thoreau@laposte.net>
2017-04-06 17:01:40 +0200

    slideshow less code

--- src/components/components/Slideshow.vue
+++ src/components/components/Slideshow.vue
@@ -2,7 +2,7 @@
   <div id="slideshow">
     <ul>
       <li v-for="(p, i) in pictures">
-        <img v-bind:src="p.url" v-bind:alt="p.alt" @click="setCurrent(i)" v-bind:class="'picture ' + (p.active ? 'active' : '')">
+        <img v-bind:src="p.url" v-bind:alt="p.alt" @click="setCurrent(i)" v-bind:class="i === currentPictureIndex ? 'active' : ''">
       </li>
     </ul>
     <img v-bind:src="pictures[currentPictureIndex].url" v-bind:alt="pictures[currentPictureIndex].alt" id="show">
@@ -17,17 +17,14 @@
         pictures: [{
           url: 'http://cdn.tutorialzine.com/wp-content/uploads/2016/03/css-variables.jpg',
           alt: 'beautiful picture',
-          active: true,
         },
         {
           url: 'http://cdn.tutorialzine.com/wp-content/uploads/2016/02/great-looking-pricing-tables-150x150.jpg',
           alt: 'great picture',
-          active: false,
         },
         {
           url: 'http://cdn.tutorialzine.com/wp-content/uploads/2015/12/creating-your-first-desktop-app-with-electron.png',
           alt: 'bad picture',
-          active: false,
         }],
         currentPictureIndex: 0,
       };
@@ -35,12 +32,6 @@
     methods: {
       setCurrent(x) {
         this.currentPictureIndex = x;
-        this.pictures.forEach((pic) => {
-          if (pic.active) {
-            pic.active = false;
-          }
-        });
-        this.pictures[x].active = true;
       },
     },
   };
@@ -60,14 +51,14 @@
     li{
       list-style-type:none;
       display:inline-block;
-    }
-    .picture{
-      width:40px;
-      height:40px;
-      object-fit:cover;
-      box-sizing:border-box;
-      &.active{
-        border:4px solid #00aadd;
+      img{
+        width:40px;
+        height:40px;
+        object-fit:cover;
+        box-sizing:border-box;
+        &.active{
+          border:4px solid #00aadd;
+        }
       }
     }
   }
