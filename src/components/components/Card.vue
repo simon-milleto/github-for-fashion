27be1383af9902eb42b2a9ae643040389cde6ebf
Clément Ollivier <clement.o2p@gmail.com>
2017-04-06 14:19:47 +0200

    Updated router link name on card.vue

--- src/components/components/Card.vue
+++ src/components/components/Card.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="mdc-layout-grid__cell mdc-layout-grid__cell--span-4">
-        <router-link :to="{ name: 'garment-detail', params: { id: data.id }}">
+        <router-link :to="{ name: 'Garment Detail', params: { id: data.id }}">
             <div class="mdc-card">
                 <div class="mdc-card__media">
                     <img src="http://pukkaind.biz/images/categories/16Wool%20varsity%20Letterman%20jacket%20Royal-white.jpg" width='100%' height='240'>
