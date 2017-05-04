2874fa4d213f1034b5010f74f1e026ce70789b4b
ECV-BO-I004-IM016 <ECV-BO-I004-IM016@ECV-BO-I004-IM016.local>
2017-05-04 12:00:34 +0200

    If empty name => displaying login

--- src/components/GHeader.vue
+++ src/components/GHeader.vue
@@ -10,10 +10,13 @@
         <section class="mdc-toolbar__section mdc-toolbar__section--align-end" role="toolbar">
           <router-link :to="{name: 'create'}" class="material-icons">add</router-link>
         </section>
-        <section v-if="state.name" class="mdc-toolbar__section mdc-toolbar__section--align-start">
-          <span>{{ state.name || state.login }}</span>
+        <section v-if="state.name != null" class="mdc-toolbar__section mdc-toolbar__section--align-start">
+          <span>{{ state.name }}</span>
         </section>
-        <section v-if="state.name" class="mdc-toolbar__section mdc-toolbar__section--align-start">
+        <section v-if="state.name == null" class="mdc-toolbar__section mdc-toolbar__section--align-start">
+          <span>{{ state.login }}</span>
+        </section>
+        <section v-if="state.name || state.login" class="mdc-toolbar__section mdc-toolbar__section--align-start">
            <a @click.prevent="disconnect" href="#">Logout</a>
         </section>
         <section v-else class="mdc-toolbar__section mdc-toolbar__section--align-start" id="login">
@@ -63,7 +66,7 @@ export default {
     /* eslint-disable object-shorthand */
     /* eslint-disable func-names */
     logininput: function (val) {
-      if (val.length !== 0) {
+      if (val.length >= 40) {
         this.loader = true;
         this.token = val.trim();
 
@@ -72,11 +75,14 @@ export default {
         });
 
         gh.getUser().getProfile().then((user) => {
-          sessionStore.setLogin(val.trim(), user.data.login, user.data.name);
-          this.state.name = user.data.name;
-          this.state.login = user.data.login;
-          this.showLogin = false;
-          this.loader = false;
+            sessionStore.setLogin(val.trim(), user.data.login, user.data.name);
+            this.state.name = user.data.name;
+            console.log("this.state.name", this.state.name);
+            this.state.login = user.data.login;
+            this.showLogin = false;
+            this.loader = false;
+
+          
         });
       }
     },
